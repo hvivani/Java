76a83e19266eeca2a92faebc08c831e33eb058aa
Hernan Vivani <hernan@vivani.com.ar>
2014-10-11 12:33:54 +0200

    Update TestBug8028111.java

--- TestBug8028111.java
+++ TestBug8028111.java
@@ -1,6 +1,7 @@
 //This app test bug:
 //https://bugs.openjdk.java.net/browse/JDK-8028111
 //hvivani-2014-10-04
+//This will raise an exception when reaching 64000 expansions if the Java version does not support it.
 
 import java.io.ByteArrayInputStream;
 
@@ -14,7 +15,7 @@ public class TestBug8028111 {
 
         XMLInputFactory factory = XMLInputFactory.newInstance();
 
-        for (int i = 0; i < 640000; i++) {
+        for (int i = 0; i < 64000; i++) {
 
             ByteArrayInputStream stream = new ByteArrayInputStream(xml.getBytes());
             factory.createXMLEventReader(stream);
